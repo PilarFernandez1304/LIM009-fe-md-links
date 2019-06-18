dc0f284da62d79898297970687213ded0bf37ec5
PilarFernandez1304 <ccentepilar@gmail.com>
2019-06-18 00:49:52 -0500

    primeras funciones

---
+++ src/cli.js
@@ -0,0 +1,23 @@
+const {validateAbsolutePath,validateIfisDirectory,readDirectory,validateIfisFile}= require('./valideRoute')
+
+//route='/home/pilar/Escritorio/LIM009-fe-md-links/'
+route='./cli.js'
+
+validateAbsolutePath(route)
+    .then(validatedRoute=>{     
+    console.log(`${validatedRoute},la ruta ahora ya es absoluta`)
+    return validateIfisDirectory(validatedRoute)
+    })
+    .then(e=>console.log('el directorio es',e ))
+    .catch(resp=>console.log(`${resp},la ruta es absoluta`))
+
+
+validateIfisFile(route)
+.then(r=>console.log(r))
+
+
+readDirectory(route)
+.then(e=>console.log(e))
+.catch(e=>console.log(e))
+
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,32 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from "../src/lib/index";
+import { validateAbsolutePath, validateIfisDirectory } from "../src/valideRoute.js";
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
+
+describe('validateAbsolutePath', () => {
+    it('validateAbsolutePath debería ser una función', () => {
+      expect(typeof validateAbsolutePath).toBe('function');
+    });
+
+    it('deberia retornar /home/pilar/Escritorio/LIM009-fe-md-links/src/LIM009-fe-md-links, si la ruta añadida no es Absoluta', () => {
+      return expect(validateAbsolutePath('./LIM009-fe-md-links')).resolves.toBe('/home/pilar/Escritorio/LIM009-fe-md-links/LIM009-fe-md-links')
+    });
+    it('deberia retornar /home/pilar/Escritorio/LIM009-fe-md-links/src/LIM009-fe-md-links, si la ruta añadida es Absoluta', () => {
+      return expect(validateAbsolutePath('/home/pilar/LIM009-fe-md-links')).rejects.toBe('/home/pilar/LIM009-fe-md-links')
+    });
 });
+
+
+
+describe('validateIfisFile', () => {
+
+    it('validateIfisFile deberia ser una funcion', () => {
+      expect(typeof validateIfisDirectory).toBe('function');
+    })
+    it('deberia retornar true si /home/pilar/Escritorio/LIM009-fe-md-links/src/ es un directorio', () => {
+      return expect(validateIfisDirectory('/home/pilar/Escritorio/LIM009-fe-md-links/src/')).resolves.toBe(true)
+    })
+    it('deberia retornar,Error: ENOENT: no such file or directory, lstat ./cli.js si no reconoce que es un directorio', () => {
+      return expect(validateIfisDirectory('./cli.js')).rejects.toBe("Error: ENOENT: no such file or directory, lstat './cli.js'")
+    })
+    
+})
