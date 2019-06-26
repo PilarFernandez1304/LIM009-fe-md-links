9e685091d9245b3a9ac6247867fe592f672391f8
PilarFernandez1304 <ccentepilar@gmail.com>
2019-06-26 13:51:17 -0500

    funcion validar links terminada

--- Prueba/cli.md
+++ Prueba/cli.md
@@ -1,8 +1,8 @@
 
 - [Node.js y npm](https://www.genbeta.com/desarrollo/node-js-y-npm)
 - [Módulos, librerías, paquetes, frameworks... ¿cuál es la diferencia?](http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175)
-- [Asíncronía en js](https://carlosazaustre.com/manejando-la-asincronia-en-javascript/)
-- [NPM](https://docs.npmjs.com/getting-started/what-is-npm)
+- [Asíncronía en js](https://carlosazaust.com/manejando-la-asincronia-en-javascript/)
+- [NPM](https://docs.np.com/getting-started/what-is-npm)
 - [Publicar packpage](https://docs.npmjs.com/getting-started/publishing-npm-packages)
 - [Crear módulos en Node.js](https://docs.npmjs.com/getting-started/publishing-npm-packages)
 
--- src/validateLinks.js
+++ src/validateLinks.js
@@ -1,36 +1,40 @@
-const {extracLinks} = require('./md-links.js');
+const { extracLinks } = require('./md-links.js');
 const fetch = require('node-fetch');
 
-fileMd=[ '/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/cli.js',
-'/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/validate.js',
-'/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/validate.md',
-'/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/cli.md' ]
+fileMd = ['/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/cli.js',
+    '/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/validate.js',
+    '/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/validate.md',
+    '/home/pilar/Escritorio/LIM009-fe-md-links/Prueba/cli.md']
 
-const validateLinks=(routesMd)=>{
-    let objLinks= extracLinks(routesMd);
-    let runObjLinks=objLinks.map((linksVal)=>new Promise((resolve)=>{
-    let linksHref=fetch(linksVal.href);
+const validateLinks = (routesMd) => {
+    let objLinks = extracLinks(routesMd);
+    let runObjLinks = objLinks.map((linksVal) => new Promise((resolve) => {
+        let linksHref = fetch(linksVal.href);
         return linksHref
-        .then((res)=>{
-            if(res.status >= 200 && res.status < 400){
-                linksVal.status = res.status;
-                linksVal.message = res.statusText;
-                console.log(linksVal)
+            .then((res) => {
+                if (res.status >= 200 && res.status < 400) {
+                    linksVal.status = res.status;
+                    linksVal.message = res.statusText;
+                    resolve(linksVal);
+                } else {
+                    linksVal.status = res.status;
+                    linksVal.message = 'Fail';
+                    resolve(linksVal);
+                }
+            })
+            .catch(error=>{
+                linksVal.status = 'No existe';
+                linksVal.message = 'Fail';
                 resolve(linksVal);
-            }
+            })
         })
-
-        }
     )
-    )
-    
-
-    return runObjLinks
+    return Promise.all(runObjLinks)
 }
 
 
-console.log(validateLinks(fileMd));
+validateLinks(fileMd).then(res=>console.log(res));
 
-module.exports={
+module.exports = {
     validateLinks
 }
